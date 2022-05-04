5215c9debb883c5a5e9161a537b64943cbc07ac4
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-04 19:48:09 -0400

    Task: Validating the Registration Form's Username

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,7 @@
 import pytest
 from app.db.models import User
 
+
 def test_nav_bar_authentication_links(client):
     """This makes the index page"""
     response = client.get("/")
@@ -31,3 +32,13 @@ def test_login_form(client):
             assert 'Login Successful' in response.data
     elif User.email is None:
         assert 'Invalid username' in response.data
+
+
+def test_registration_form_email(client):
+    """ Unit Test for Invalid Email for Registration"""
+    response = client.post("/register")
+    test_email = 'test'
+    if User.email is None:
+        User.email = test_email
+        if '@' not in User.email:
+            assert 'Please include an ' @ ' in the email address' in response.data
