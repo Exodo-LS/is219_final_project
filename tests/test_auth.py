5a194e5aed1e98ea5f1aeade98b7078d1d28ccb3
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-04 19:38:22 -0400

    Task: Testing the Templates in the Auth Directory generate a response

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,3 +8,12 @@ def test_nav_bar_authentication_links(client):
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
+
+def test_templates_in_auth_directory(client):
+    """This makes the index page"""
+    response = client.get("/dashboard")
+    assert response.status_code == 302
+    response = client.get("/register")
+    assert response.status_code == 200
+    response = client.get("/login")
+    assert response.status_code == 200
