320581563fc27b8cb88424cefc3e44ff6db907f4
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-04 19:45:41 -0400

    Task: Validating the Login Form's Username and Password

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,5 @@
 import pytest
-
+from app.db.models import User
 
 def test_nav_bar_authentication_links(client):
     """This makes the index page"""
@@ -17,3 +17,17 @@ def test_templates_in_auth_directory(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+
+def test_login_form(client):
+    """ Unit Test for Incorrect Password for Login """
+    response = client.post("/login")
+    test_user = 'IS219_TestUser@email.com'
+    test_password = 'invalid_password'
+    if User.email == test_user:
+        if test_password != User.password:
+            assert 'Invalid password' in response.data
+        else:
+            assert 'Login Successful' in response.data
+    elif User.email is None:
+        assert 'Invalid username' in response.data
