c01159a4ed382dda06ac8f9d65d9c0babb57d000
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-05-04 19:36:28 -0400

    Task: Testing The Navbar Can Send to Register/Login

---
+++ tests/test_auth.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_nav_bar_authentication_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
+
